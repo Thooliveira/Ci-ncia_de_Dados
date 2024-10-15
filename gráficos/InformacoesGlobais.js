4902835fefa197e03cdb2aa537f67e5413545f2e
Thooliveira <165859202+Thooliveira@users.noreply.github.com>
2024-10-15 19:47:55 +0000

    atualização 15.10

---
+++ gráficos/InformacoesGlobais.js
@@ -0,0 +1,11 @@
+https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+
+    const res = await fetch(url)
+
+    const dados = await res.json()
+
+    console.log(dados)
+
+}
